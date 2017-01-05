fee4ac257f80cd34ee46a4cd4b1e6d1c7b3e4556
Kyle Niemeyer <kyle.niemeyer@gmail.com>
2017-01-05 12:03:23 -0800

    updated CPU scaling plots
    x-axis with number of CPU cores now linear axis rather than log, and
    made the legend consistent with other plots.

--- plotting_scripts/plot_cpu_scaling.py
+++ plotting_scripts/plot_cpu_scaling.py
@@ -21,10 +21,10 @@ legend_markers = ['o', 'v', 's', '>']
 legend_colors = ['b', 'g', 'r', 'c']
 
 # x position of text label, and multiplier for y position
-label_locs = [(10, 1.8),
-              (10, 0.45),
-              (10, 1.85),
-              (10, 1.8)
+label_locs = [(16, 2.2),
+              (16, 0.75),
+              (16, 2.2),
+              (16, 2.2)
               ]
 
 
@@ -40,14 +40,14 @@ def __plot(plotdata, outname):
     #convert to evaluation time / condition
     #and # of threads
     for pd in plotdata:
-      pd.y = [y / pd.x for y in pd.y]
-      pd.x = pd.num_threads
+        pd.y = [y / pd.x for y in pd.y]
+        pd.x = pd.num_threads
 
     minx, miny = plot_scaling(plotdata, legend_markers, legend_colors,
                               label_locs=label_locs
                               )
     ax.set_yscale('log')
-    ax.set_xscale('log')
+    #ax.set_xscale('log')
     ax.set_ylim(ymin=miny*0.85)
     ax.set_xlim(xmin=minx*0.85, xmax=35)
     #ax.legend(loc=0, numpoints=1, frameon=False)
@@ -65,10 +65,12 @@ def __plot(plotdata, outname):
     #draw scaling lines
     for mech in set(x.mechanism for x in plotdata):
         per_mech = [x for x in plotdata if x.mechanism == mech
-            and x.x <= cutoff]
+                    and x.x <= cutoff
+                    ]
         per_mech = sorted(per_mech, key=lambda x: x.x)
         eff = [np.mean(per_mech[0].y) / (per_mech[i].x * np.mean(per_mech[i].y))
-            for i in range(len(per_mech))]
+               for i in range(len(per_mech))
+               ]
         eff_x = [per_mech[i].x for i in range(len(per_mech))]
         per_mech = [copy.copy(pd) for pd in per_mech[:]]
         for i in range(len(per_mech)):
@@ -78,13 +80,16 @@ def __plot(plotdata, outname):
 
     fig, ax = plt.subplots()
     minx, miny = plot_scaling(scalings, legend_markers, legend_colors, plot_std=False)
-    ax.legend(loc=0)
-    ax.set_ylim(ymin=miny*0.98, ymax=1.05)
+    #ax.legend(loc=0)
+    #ax.set_xscale('log')
+    ax.set_ylim(ymin=miny*0.98, ymax=1.02)
     ax.set_xlim(xmin=0, xmax=35)
 
-    #ax.legend(loc=0, numpoints=1, frameon=False)
+    ax.legend(loc=0, numpoints=1, fontsize=font_size,
+              shadow=True, fancybox=True
+              )
     # add some text for labels, title and axes ticks
-    ax.set_ylabel('Parallel Scaling Efficiency', fontsize=font_size)
+    ax.set_ylabel('Parallel scaling efficiency', fontsize=font_size)
     ax.set_xlabel('Number of CPU threads', fontsize=font_size)
     ind = outname.index('.pdf')
     outname = outname[:ind] + '_par' + '.pdf'
